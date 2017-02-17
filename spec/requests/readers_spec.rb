cf71df7d2444be79a8102d4251423eae9828e257
Sofistication <sordonez93@gmail.com>
2017-02-16 19:56:58 -0500

    Fix linter errors
    - edited automatically generated rspec files

--- spec/requests/readers_spec.rb
+++ spec/requests/readers_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Readers", type: :request do
-  describe "GET /readers" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Readers', type: :request do
+  describe 'GET /readers' do
+    it 'works! (now write some real specs)' do
       get readers_path
       expect(response).to have_http_status(200)
     end
--- spec/routing/readers_routing_spec.rb
+++ spec/routing/readers_routing_spec.rb
@@ -1,38 +1,55 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe ReadersController, type: :routing do
-  describe "routing" do
-
-    it "routes to #index" do
-      expect(:get => "/readers").to route_to("readers#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get('/readers')).to route_to('readers#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/readers/new").to route_to("readers#new")
+    # it 'routes to #new' do
+    #   expect(:get => '/readers/new').to route_to('readers#new')
+    # end
+
+    it 'routes to #show' do
+      expect(get('/readers/1')).to route_to(
+        controller: 'readers',
+        action: 'show',
+        id: '1'
+      )
     end
 
-    it "routes to #show" do
-      expect(:get => "/readers/1").to route_to("readers#show", :id => "1")
+    # it 'routes to #edit' do
+    #   expect(:get => '/readers/1/edit').to route_to('readers#edit', :id => '1')
+    # end
+
+    it 'routes to #create' do
+      expect(post('/readers')).to route_to('readers#create')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/readers/1/edit").to route_to("readers#edit", :id => "1")
+    it 'routes to #update via PUT' do
+      expect(put('/readers/1')).to route_to(
+        controller: 'readers',
+        action: 'update',
+        id: '1'
+      )
     end
 
-    it "routes to #create" do
-      expect(:post => "/readers").to route_to("readers#create")
+    it 'routes to #update via PATCH' do
+      expect(patch('/readers/1')).to route_to(
+        controller: 'readers',
+        action: 'update',
+        id: '1'
+      )
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/readers/1").to route_to("readers#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/readers/1").to route_to("readers#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/readers/1").to route_to("readers#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete('/readers/1')).to route_to(
+        controller: 'readers',
+        action: 'destroy',
+        id: '1'
+      )
     end
 
   end

b74e28c237852b550896cd77fce508c3994a2cab
Sofistication <sordonez93@gmail.com>
2017-02-16 20:33:59 -0500

    Migrate db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216034950) do
+ActiveRecord::Schema.define(version: 20170217012455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20170216034950) do
     t.text     "description"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "list"
+    t.integer  "reader_id",  null: false
+    t.integer  "book_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_entries_on_book_id", using: :btree
+    t.index ["reader_id"], name: "index_entries_on_reader_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -49,5 +59,7 @@ ActiveRecord::Schema.define(version: 20170216034950) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "entries", "books"
+  add_foreign_key "entries", "readers"
   add_foreign_key "examples", "users"
 end

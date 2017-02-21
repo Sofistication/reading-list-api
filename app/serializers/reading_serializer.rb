b065f89f6b493b77c87e67b8953f4a0ce9940177
Sofistication <sordonez93@gmail.com>
2017-02-21 09:56:13 -0500

    Implement readings in list action
    - changed `current_user.books` to `current_user.readings`
    - added book attribute in serializer
    - USE OF READING INTRODUCES BREAKING CHANGES

--- app/serializers/reading_serializer.rb
+++ app/serializers/reading_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class ReadingSerializer < ActiveModel::Serializer
-  attributes :id, :list, :status
+  attributes :id, :list, :status, :book
   has_one :user
   has_one :book
+
+  def book
+    object.book
+  end
 end
